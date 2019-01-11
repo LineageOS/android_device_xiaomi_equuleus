24ff848d9bb1298abc0eaf4dc3df8c31411021eb
Bruno Martins <bgcngm@gmail.com>
2019-01-11 23:04:15 +0000

    equuleus: Add device-specific fstab and flags
     * This got decommonized in order to support perseus,
       which is a system-as-root device.
    
    Change-Id: I0eba72337e7f190d86c37aa6aff893fdac61975e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,5 +15,11 @@ TARGET_OTA_ASSERT_DEVICE := equuleus
 # Kernel
 TARGET_KERNEL_CONFIG := equuleus_defconfig
 
+# Partitions
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
 # Inherit from the proprietary version
 -include vendor/xiaomi/equuleus/BoardConfigVendor.mk
