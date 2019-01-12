11022d6a10e1b1e0eb6533bc2057e57ef4364bfb
Bruno Martins <bgcngm@gmail.com>
2019-01-12 10:12:07 +0000

    equuleus: Add stock system props
    Change-Id: Id36e4369df049e750a9887daf09678f92cffc3d0

---
+++ device-props.mk
@@ -0,0 +1,16 @@
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.sat.fallback.dist=45 \
+    persist.camera.sat.fallback.dist.d=5 \
+    persist.camera.sat.fallback.luxindex=310 \
+    persist.camera.sat.fallback.lux.d=50
+
+# Display density
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=440
+
+# Display features
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.displayfeature.histogram.enable=false \
+    ro.eyecare.brightness.threshold=3 \
+    ro.eyecare.brightness.level=8
