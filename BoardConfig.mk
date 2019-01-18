bfdc1c16eed8fc90490156aa5dd67f047bfd1d46
Anton Hansson <hansson@google.com>
2019-01-18 14:10:45 +0000

    equuleus: Use TARGET_SCREEN_DENSITY
    This variable is used to auto-populate the ro.sf.lcd_density as
    well as selecting density bucket for resources in recovery.
    
    Bug: 118676832
    Test: adb shell getprop ro.sf.lcd_density
    Test: boot into recovery
    Change-Id: Ib383d18b61e7a2058a69ae26136aa053fdba4df2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,9 @@ DEVICE_PATH := device/xiaomi/equuleus
 # Assert
 TARGET_OTA_ASSERT_DEVICE := equuleus
 
+# Display
+TARGET_SCREEN_DENSITY := 440
+
 # Kernel
 TARGET_KERNEL_CONFIG := equuleus_defconfig
 
