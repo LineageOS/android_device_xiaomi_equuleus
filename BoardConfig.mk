7f4b1cc250056c80fe6a781028a62de3e29cbae3
Giuseppe Barillari <joe2k01dev@gmail.com>
2019-02-25 21:16:10 +0100

    equuleus: Set tap to wake node path
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>
    Change-Id: I522b92bb23c3c980d61f0a91787d61132733971b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,9 @@ TARGET_KERNEL_CONFIG := equuleus_defconfig
 # Partitions
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/dev/input/event3"
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
