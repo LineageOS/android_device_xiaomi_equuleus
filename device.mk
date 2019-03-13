62d88e6652d376f5887ac8ec4734a959ecb3d2b2
Bruno Martins <bgcngm@gmail.com>
2019-03-13 21:00:33 +0000

    equuleus: Import stock NFC configs
     * Configs taken from MIUI 9.6.20 global dev build
    
    Change-Id: Ie8149f4990fa85c0bb9ad7f6338c646d50b4d33b

--- device.mk
+++ device.mk
@@ -49,3 +49,7 @@ PRODUCT_PACKAGES += \
     NQNfcNci \
     SecureElement \
     Tag
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
