58a3d42dfddd262e640a23f29c09e044aab93236
Bruno Martins <bgcngm@gmail.com>
2020-02-18 23:49:20 +0000

    equuleus: Split system and vendor props
    Change-Id: If965be42e68e9ebbd816656dd2af79f541c1ceea

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,10 @@ TARGET_KERNEL_CONFIG := equuleus_defconfig
 # Partitions
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
