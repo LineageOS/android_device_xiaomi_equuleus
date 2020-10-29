c823daa4b3810e074b996d615fa6efbcc9f939c1
Alessandro Astone <ales.astone@gmail.com>
2020-10-29 08:40:54 +0100

    equuleus: Define a soong namespace
    Change-Id: Ida759ee1786c667e766da29103f0ecb54950c104

--- device.mk
+++ device.mk
@@ -106,6 +106,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     sensors.udfps
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # WiFi
 PRODUCT_PACKAGES += \
     TargetWifiOverlay
