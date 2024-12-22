4772057e56f1adeb22a82807a33977a4b77b1cba
Bruno Martins <bgcngm@gmail.com>
2024-12-22 15:33:58 +0000

    equuleus: Convert gps.conf symlink to an install_symlink target
    Change-Id: Ic62ad3d25de82b09574a85d0c4897232a9b36f12

--- device.mk
+++ device.mk
@@ -79,6 +79,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/gps_debug.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/gps_debug.conf
 
+PRODUCT_PACKAGES += \
+    gps.conf_symlink
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service.xiaomi_sdm845
